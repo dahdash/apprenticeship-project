6dd53a517fa3a52a878205729cd5e98afb67bd2c
dahdash <dahdash@gmail.com>
2019-05-13 18:38:27 +0430

    define user and task relations in models

--- app/Task.php
+++ app/Task.php
@@ -12,4 +12,10 @@ class Task extends Model
      * @var array
      */
     protected $fillable = ['name'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
 }
